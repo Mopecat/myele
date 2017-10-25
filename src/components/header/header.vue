369a829e251807bf05d8edebc35b5e311154b9f8
Mopecat <liuyangdiv@163.com>
2017-10-25 10:30:24 +0800

    head组件更新部分

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,12 +17,24 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
           <span class="count">{{seller.supports.length}}个</span>
           <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main"></div>
+      </div>
+      <div class="detail-close"></div>
+    </div>
   </div>
 </template>
 
@@ -31,6 +43,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+      return {
+        detailShow: false
+      };
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true;
       }
     },
     created() {
@@ -42,8 +64,10 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin"
   .header
+    position: relative
     color: #fff
-    background: #999
+    background: rgba(7,17,27,0.5)
+    overflow: hidden
     .content-wrapper
       position: relative
       padding: 24px 12px 18px 24px
@@ -121,4 +145,48 @@
           margin-left: 2px
           line-height: 24px
           font-size: 10px
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7,17,27,0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        width: 22px
+        height: 12px
+        margin-top: 7px
+        bg-img('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        font-size: 10px
+        vertical-align: top
+        margin: 0 4px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 9px
+
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+    .detail
+      position: fixed
+      top: 0
+      width: 100%
+      height: 100%
+      z-index: 100
+      overflow: auto
+      background: rgba(7,17,27,0.8)
 </style>
